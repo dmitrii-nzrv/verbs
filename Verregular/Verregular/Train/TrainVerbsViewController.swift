a5883ef7493d9558bc4d7d408951c9add8493ce2
Dmitrii <dmitrii.nazarov7703@gmail.com>
2023-12-11 21:16:37 +0300

    configure train screen

--- Verregular/Verregular/Train/TrainVerbsViewController.swift
+++ Verregular/Verregular/Train/TrainVerbsViewController.swift
@@ -6,14 +6,156 @@
 //
 
 import UIKit
+import SnapKit
 
 final class TrainVerbsViewController: UIViewController {
+    
+    //  MARK: ~ GUI Variables
+    private lazy var scrollView: UIScrollView = {
+        let view = UIScrollView()
+        
+        view.showsHorizontalScrollIndicator = false
+        
+        return view
+    }()
+    
+    private lazy var contentView: UIView = UIView()
+    
+    private lazy var infinitiveLabel: UILabel = {
+        let label = UILabel()
+        
+        label.font = .boldSystemFont(ofSize: 28)
+        label.textColor = .black
+        label.textAlignment = .center
+        label.text = "Read".uppercased()
+        
+        return label
+    }()
+    
+    private lazy var pastSimpleLabel: UILabel = {
+        let label = UILabel()
+        
+        label.font = .systemFont(ofSize: 14)
+        label.textColor = .gray
+        label.text = "Past simple"
+        
+        return label
+    }()
+    
+    private lazy var pastParticipleLabel: UILabel = {
+        let label = UILabel()
+        
+        label.font = .systemFont(ofSize: 14)
+        label.textColor = .gray
+        label.text = "Past Participle"
+        
+        return label
+    }()
+    
+    private lazy var pastSimpleTextField: UITextField = {
+        let field = UITextField()
+        
+        field.borderStyle = .roundedRect
+        field.delegate = self
+        
+        return field
+    }()
+    
+    private lazy var pastParticipleField: UITextField = {
+        let field = UITextField()
+        
+        field.borderStyle = .roundedRect
+        field.delegate = self
+        
+        return field
+    }()
+    
+    private lazy var checkButton: UIButton = {
+        let button = UIButton()
+        
+        button.layer.cornerRadius = 10
+        button.backgroundColor = .systemGray5
+        button.setTitle("Check".localized, for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        
+        return button
+    }()
+    
+    // MARK: ~ Properties
+    private let edgeInsets = 30
     
     // MARK: ~ Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
         title = "Train verbs".localized
-        view.backgroundColor = .red
+        
+        
+        setupUI()
     }
+    
+    // MARK: ~ Private methods
+    private func setupUI() {
+        view.backgroundColor = .white
+        
+        view.addSubview(scrollView)
+        scrollView.addSubview(contentView)
+        contentView.addSubviews([
+            infinitiveLabel,
+            pastSimpleLabel,
+            pastSimpleTextField,
+            pastParticipleLabel,
+            pastParticipleField,
+            checkButton])
+        
+        setupConstraints()
+    }
+    
+    private func setupConstraints() {
+        scrollView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+        
+        contentView.snp.makeConstraints { make in
+            make.size.edges.equalToSuperview()
+        }
+        
+        infinitiveLabel.snp.makeConstraints { make in
+            make.top.equalToSuperview().inset(200)
+            make.trailing.leading.equalToSuperview().inset(edgeInsets)
+        }
+        
+        pastSimpleLabel.snp.makeConstraints { make in
+            make.top.equalTo(infinitiveLabel.snp.bottom).offset(60)
+            make.trailing.leading.equalToSuperview().inset(edgeInsets)
+        }
+        
+        pastSimpleTextField.snp.makeConstraints { make in
+            make.top.equalTo(pastSimpleLabel.snp.bottom).offset(10)
+            make.trailing.leading.equalToSuperview().inset(edgeInsets)
+        }
+        
+        pastParticipleLabel.snp.makeConstraints { make in
+            make.top.equalTo(pastSimpleTextField.snp.bottom).offset(20)
+            make.trailing.leading.equalToSuperview().inset(edgeInsets)
+        }
+        
+        pastParticipleField.snp.makeConstraints { make in
+            make.top.equalTo(pastParticipleLabel.snp.bottom).offset(10)
+            make.trailing.leading.equalToSuperview().inset(edgeInsets)
+        }
+        
+        checkButton.snp.makeConstraints { make in
+            make.top.equalTo(pastParticipleField.snp.bottom).offset(100)
+            make.trailing.leading.equalToSuperview().inset(edgeInsets)
+        }
+    }
+}
+
+    
+
+
+// MARK: ~ UITextFieldDelegate
+extension TrainVerbsViewController: UITextFieldDelegate {
+    // TODO:
 }
